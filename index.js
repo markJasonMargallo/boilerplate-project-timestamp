591820f58cfa495b4866093b2c2e437378a6aa1c
markJasonMargallo <201910681@gordoncollege.edu.ph>
2023-04-06 12:43:28 +0800

    implement time route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,13 +19,27 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:timestamp", function (req, res) {
 
+  var valid = (new Date(req.params.timestamp)).getTime() > 0;
+  var timestamp = new Date(req.params.timestamp).getTime() / 1000;
+
+  if(!valid){
+    var date = new Date(req.params.timestamp * 1000);
+    res.json({unix: req.params.timestamp, utc: date.toUTCString()});
+    console.log("timestamp")
+  }else{
+    var date = new Date(timestamp * 1000);
+    res.json({unix: timestamp, utc: date.toUTCString()});
+    console.log("date string")
+  }
+  
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
